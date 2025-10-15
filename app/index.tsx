91420233c2fdd22b0b589f862e1e22b635ba8cf4
Savinda De Silva <siwanthakasavinda@gmail.com>
2025-10-15 17:40:00 +0530

    NativeWind Setup

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import "./global.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl text-center font-bold text-black-500 font-quicksand-bold">
+        Welcome to My React native app!
       </Text>
     </View>
   );
