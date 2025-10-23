6343113107751fd8807b9eca05f7a94e5dbe4766
Savinda De Silva <siwanthakasavinda@gmail.com>
2025-10-23 15:49:20 +0530

    setup sign in page and more works

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,36 @@
-import { router } from 'expo-router'
-import { Button, Text, View } from 'react-native'
+import CustomButton from '@/components/CustomButton'
+import CustomInput from '@/components/CustomInput'
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-      <Button title="Sign Up" onPress={() => router.push("/sign-up")} />
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5 ">
+         <CustomInput
+                    placeholder="Enter your email" 
+                    value={''}
+                    onChangeText={() => {}}
+                    label="Email"
+                    keyboardType="email-address"
+                  />
+          <CustomInput
+                    placeholder="Enter your Password" 
+                    value={''}
+                    onChangeText={() => {}}
+                    label="Password"
+                    secureTextEntry={true}
+                  />
+          <CustomButton 
+                    title="Sign In"
+                  />
+          <View className="flex justify-center mt-5 flex-row gap-2 ">
+              <Text className="base-regular text-gray-100">
+                Don't have an account?
+              </Text>
+              <Link href="/sign-up" className="base-bold text-primary">
+                Sign Up
+              </Link>
+          </View>
     </View>
   )
 }
---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    Platform: "com..foodordering.MYFood",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68f64725003923b0694f',
+    userCollectionId: 'user'
+}
