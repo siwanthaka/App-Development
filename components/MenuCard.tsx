b72ef050c25cecd7234dd3dc93ac05987f90479c
Savinda De Silva <siwanthakasavinda@gmail.com>
2025-11-08 15:40:47 +0530

    working on search page

---
+++ components/MenuCard.tsx
@@ -0,0 +1,19 @@
+import { MenuItem } from '@/type';
+import { Image, Text, TouchableOpacity } from 'react-native';
+
+const MenuCard = ({item: {image_url, name, price}}: {item: MenuItem}) => {
+    const imageUrl = image_url;
+
+
+  return (
+    <TouchableOpacity>
+      <Image source={{uri:imageUrl}} className="size-32 absolute -top-10" resizeMode="contain"/>
+      <Text className='text-center base-bold text-dark-100 mb-2' numberOfLines={1}>{name}</Text>
+      <Text className='body-regular text-gray-200 mb-4'>From ${price}</Text>
+      <TouchableOpacity onPress={() => {}}></TouchableOpacity>
+        <Text className='paragraph-bold text-primary'>Add to Cart +</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default MenuCard
